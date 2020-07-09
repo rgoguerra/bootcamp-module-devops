2be924930ae27d95737bfcdc67575d3ea1bc5eb7
EC2 Default User <ec2-user@ip-172-31-28-159.ec2.internal>
2020-07-09 01:48:49 +0000

    Modificar user data taller 2

--- talleres/02/user-data.sh
+++ talleres/02/user-data.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -xe
 
-yum install -y python37
+exec > >(tee /var/log/cloud-init-output.log|logger -t user-data -s 2>/dev/console) 2>&1
+
+yum install -y python37 git
 curl -O https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py
-git clone https://github.com/eloyvega/bootcamp-devops.git
-cd ~/bootcamp-devops/backend/
+cd ~
+git clone https://github.com/eloyvega/bootcamp-module-devops.git
+cd ~/bootcamp-module-devops/backend/
 pip install -r requirements.txt
 nohup gunicorn -w 3 -b 0.0.0.0:8000 app:app &
